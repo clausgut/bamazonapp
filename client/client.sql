0af1cd9b59bd53b8c34684fb07b6560b9c2d83fa
Claudia Gutierrez <gutierrez.cp21@yahoo.com>
2019-05-15 19:54:27 -0400

    env passwords and information

---
+++ client/client.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products(
+    item_id INTEGER NOT NULL AUTO_INCREMENT,
+
+   product_name VARCHAR(50) NOT NULL,
+
+   department_name VARCHAR(50) NOT NULL,
+
+   price DECIMAL(10,2) NOT NULL,
+
+   stock_quantity INTEGER(10) NOT NULL,
+
+   PRIMARY KEY (item_id)
+);
+
